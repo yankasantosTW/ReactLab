eddcb528826f15cfbce32fd0f35768eee9ebcf3a
yankasantosTW <yanka.santos@thoughtworks.com>
2024-09-02 18:35:28 -0300

    Add a fetch to get the places from API

--- section15-sending-http-request/src/components/AvailablePlaces.jsx
+++ section15-sending-http-request/src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  //fetch it's a promise that returns a response object with data from API calls
+  fetch('http://localhost:3000/places')
+  .then((response)=>{
+    return response.json();
+  })
+  .then((responseData)=>{
+    setAvailablePlaces(responseData.places);
+  });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
