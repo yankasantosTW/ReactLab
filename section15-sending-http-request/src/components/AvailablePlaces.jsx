5672af88469b696e1b4b476aa6f264a7c1e64f00
yankasantosTW <yanka.santos@thoughtworks.com>
2024-09-02 18:47:30 -0300

    Add the useEffect to call the API whem the component is mounted

--- section15-sending-http-request/src/components/AvailablePlaces.jsx
+++ section15-sending-http-request/src/components/AvailablePlaces.jsx
@@ -1,16 +1,20 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  //fetch it's a promise that returns a response object with data from API calls
-  fetch('http://localhost:3000/places')
-  .then((response)=>{
-    return response.json();
-  })
-  .then((responseData)=>{
-    setAvailablePlaces(responseData.places);
-  });
+  //Using the UseEffect to to fetch the data once the component is mounted
+  useEffect(()=> {
+    //fetch it's a promise that returns a response object with data from API calls
+    fetch('http://localhost:3000/places')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((responseData)=>{
+      setAvailablePlaces(responseData.places);
+    });
+  }, []);
 
   return (
     <Places
