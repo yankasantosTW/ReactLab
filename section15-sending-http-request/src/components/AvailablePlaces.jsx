3b58c1a32a387dd3092612137525714d53fc5fd1
Yanka Santos <86688772+yankasantosTW@users.noreply.github.com>
2024-09-03 19:34:08 -0300

    Handling HTTP errors using try catch and create the isLoading state (#2)
    * Dummy back-end api and project structure
    
    * Add a fetch to get the places from API
    
    * Add the useEffect to call the API whem the component is mounted
    
    * Refectoring the fetch method to use the async await function
    
    * Adjusting to change the state of isLoading based on fetch process
    
    * Handling HTTP errors usisng try catch

--- section15-sending-http-request/src/components/AvailablePlaces.jsx
+++ section15-sending-http-request/src/components/AvailablePlaces.jsx
@@ -1,25 +1,48 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
 
   //Using the UseEffect to to fetch the data once the component is mounted
   useEffect(()=> {
     //Refectoring the fetch to create a function using the async await
     async function fetchPlaces(){
-      const response = await fetch('http://localhost:3000/places');
-      const responseData = await response.json();
-      setAvailablePlaces(responseData.places);
+      setIsFetching(true);
+      //Using the try catch to handler the error on request API
+      try{
+        const response = await fetch('http://localhost:3000/placess');
+        const responseData = await response.json();
+        //verify if the response it's received successfully
+        if(!response.ok){
+          throw new Error('Failed to fetch places');
+        }
+        setAvailablePlaces(responseData.places);
+      }
+      catch(error){
+        setError({message: error.message || 'Could not fetch places. Try again later.'});
+      }
+      
+      setIsFetching(false);
     }
-
+    
     fetchPlaces();
   }, []);
+  //Render the error message if the request fail
+  if (error) {    
+    return <ErrorPage title="An error occurred!" message={error.message} />;
+  }        
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
