551c1526f86f993be11b3d3c222268e31374b3f6
yankasantosTW <yanka.santos@thoughtworks.com>
2024-09-03 18:51:14 -0300

    Adjusting to change the state of isLoading based on fetch process

--- section15-sending-http-request/src/components/AvailablePlaces.jsx
+++ section15-sending-http-request/src/components/AvailablePlaces.jsx
@@ -3,14 +3,18 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
 
   //Using the UseEffect to to fetch the data once the component is mounted
   useEffect(()=> {
     //Refectoring the fetch to create a function using the async await
     async function fetchPlaces(){
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const responseData = await response.json();
       setAvailablePlaces(responseData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -20,6 +24,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
