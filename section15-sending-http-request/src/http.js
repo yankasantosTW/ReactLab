ac7a3af13f4c2a3494639eb67f1911f5a58b188a
yankasantosTW <yanka.santos@thoughtworks.com>
2024-09-09 19:47:56 -0300

    Create the PUT request to save the selected places from the user

--- section15-sending-http-request/src/http.js
+++ section15-sending-http-request/src/http.js
@@ -7,3 +7,22 @@ export async function fetchAvailablePlaces() {
     }
     return responseData.places;
 }
+//We use the PUT method to Create and Update in the same object URL
+// We are using the distructure as {places} because the key is "places" and the value is "places"
+export async function updateUserPlaces(places){
+    const response =  await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places}),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    });
+    
+    const responseData = await response.json();
+    
+    if(!responseData.ok){
+        throw new Error('Failed to update user data.');
+    }
+    return responseData.message;
+
+}
